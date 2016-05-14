d7d08759ff26f4a533c82235f20e5dc52f4b930f
negar.qorbani <negar.qorbani@gmail.com>
2016-05-14 18:48:02 +0430

    ResourceCatalogue responsibilities are added. delete and selectAll functions are added to DataBase class

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
+import Resource.Resource;
+import Resource.ResourceCatalogue;
 import UserManagement.Employee;
 
 public class Main {
 	public static void main(String[] args) {
-		Employee emp = new Employee();
-		if(!(emp.login("ali","124"))){
-			System.out.println("huraaa");
-		}else{
-			System.out.println("shit");
-		}
-		if(emp.login("ali", "123")){
-			System.out.println(emp.getName());
-			System.out.println(emp.loggedin);
-
-		}
-		if(emp.logout()){
-			System.out.println(emp.loggedin);
-		}
+		// test employee
+//		Employee emp = new Employee();
+//		if(!(emp.login("ali","124"))){
+//			System.out.println("huraaa");
+//		}else{
+//			System.out.println("shit");
+//		}
+//		if(emp.login("ali", "123")){
+//			System.out.println(emp.getName());
+//			System.out.println(emp.loggedin);
+//
+//		}
+//		if(emp.logout()){
+//			System.out.println(emp.loggedin);
+//		}
+		
+		// test resource
+//		ResourceCatalogue rscat = new ResourceCatalogue();
+//		rscat = new ResourceCatalogue();
+////		rscat.addResource(8, "sanad8");
+//		rscat.readAllResources();
+//		rscat.deleteResource(4);
+//		System.out.println();
+//		rscat.readAllResources();
 	}
 }
--- src/Resource/ResourceCatalogue.java
+++ src/Resource/ResourceCatalogue.java
@@ -1,5 +1,46 @@
 package Resource;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import DataBase.DataBase;
+
 public class ResourceCatalogue{
+	DataBase DB;
 	
+	public ResourceCatalogue(){
+		DB = new DataBase();
+	}
+	
+	public void readAllResources(){
+		try {
+			ResultSet rs = DB.selectAll("resource");
+			while ( rs.next() ) {
+			    int id = rs.getInt("rid");
+			    String  name = rs.getString("rname");
+			    System.out.print( "ID = " + id +" ; ");
+			    System.out.println( "NAME = " + name );
+			 }
+	        rs.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addResource(int id, String name){
+		HashMap<String, String> vars = new HashMap<String, String>();
+		vars.put("rid", Integer.toString(id));
+		vars.put("rname", "\'"+name+"\'");
+		DB.insert(vars, "resource");
+	}
+
+	public void deleteResource(int id){
+		HashMap<String, String> vars = new HashMap<String, String>();
+		vars.put("rid", Integer.toString(id));
+		DB.delete(vars, "resource");
+	}
+
 }
