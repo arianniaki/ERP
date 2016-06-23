59f2c1596e7b9dd8fae8a28b4d53710e0640b10d
unknown <mmotiei@bitbucket.org>
2016-06-23 11:37:40 +0430

    checkbox is ready

--- src/GUI/Form/CheckBoxJPanel.java
+++ src/GUI/Form/CheckBoxJPanel.java
@@ -1,26 +1,46 @@
 package GUI.Form;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-public class CheckBoxJPanel extends FieldPanel {
 
+public class CheckBoxJPanel extends FieldPanel {
+	private CheckBoxTable cbt;
 	public CheckBoxJPanel(Field field) {
 		super(field.getName());
-		// TODO Auto-generated constructor stub
+		Object[][] options = new Object[field.options.size()][2];
+		String[] columns = {field.getName(),"Check Box"};
+		for (int i = 0; i < field.options.size(); i++){
+			options[i][0] = field.options.get(i);
+			options[i][1] = Boolean.FALSE;
+		}
+		cbt = new CheckBoxTable(options,columns);
+		JPanel labelPanel = new JPanel();
+		JPanel fieldPanel = new JPanel();
+		add(labelPanel, BorderLayout.WEST);
+		add(fieldPanel, BorderLayout.CENTER);
+		JLabel lab = new JLabel(field.getLabel(), JLabel.RIGHT);
+		lab.setLabelFor(cbt);
+		labelPanel.add(lab);
+		fieldPanel.add(cbt);
 	}
 
 	@Override
 	public ArrayList<String> getValues() {
-		// TODO Auto-generated method stub
-		return null;
+		return cbt.getValues();
 	}
-
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public ArrayList<String> getCheckedValues() {
+		return cbt.getSelectedValues();
 	}
 
 }
+
+
