e6075fbf6fe489e000a539f34ac6bee4b0c41141
negar.qorbani <negar.qorbani@gmail.com>
2016-06-23 09:46:32 +0430

    ResourceRequirement is done

--- src/RequirementUtilization/ResourceRequirement.java
+++ src/RequirementUtilization/ResourceRequirement.java
@@ -1,5 +1,33 @@
 package RequirementUtilization;
 
+import ProjectEmployee.Project;
+import ResourceManagement.Section.Section;
+import ResourceManagement.Section.Resource.Resource;
+
 public class ResourceRequirement{
+	int duration;
+	Project project;
+	Section section;
+	Resource resource;
+	boolean isSatisfied;
 	
+	public ResourceRequirement(int duration, Project project, Section section, Resource resource){
+		this.duration = duration;
+		this.project = project;
+		this.section = section;
+		this.resource = resource;
+		isSatisfied = false;
+	}
+	
+	public void edit(int duration, Project project, Section section, Resource resource, boolean isSatisfied){
+		this.duration = duration;
+		this.project = project;
+		this.section = section;
+		this.resource = resource;
+		this.isSatisfied = isSatisfied;
+	}
+	
+	public void satisfy(){
+		isSatisfied = true;
+	}
 }
